2568b55823433f7666a15c50fcf483368bf3c1db
Carmen Valente <carmenvalente@rentalcars.com>
2020-08-21 13:54:21 +0100

    Added cat fact retrieval functionality

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, TouchableOpacity, Alert } from 'react-native';
+import { styles } from './src/styles/home.js'
+import {CatFacts} from "./src/catFactsIndex";
 
 export default function App() {
-  return (
+    return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CatFacts/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles/home.js
@@ -0,0 +1,23 @@
+import {StyleSheet} from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#2b2b2b',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        color: '#fff',
+        fontSize: 50
+    },
+    button: {
+        backgroundColor: "#c75aa3",
+        padding: 20,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color:'#fff',
+        fontSize: 30
+    },
+});
